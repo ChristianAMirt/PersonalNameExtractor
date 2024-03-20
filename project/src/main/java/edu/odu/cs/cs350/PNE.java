650c68a8b78a105d9a641c54fecd7a63d7477697
Anthony <aakin007@odu.edu>
2024-03-20 19:30:09 -0400

    Created PNE Entry File to run the porgram.

---
+++ project/src/main/java/edu/odu/cs/cs350/PNE.java
@@ -0,0 +1,14 @@
+package edu.odu.cs.cs350;
+
+public class PNE {
+    
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.err.println("PNE.jar [input text]");
+            System.exit(1);
+        }
+        String inputText = String.join(" ", args);
+        Document doc = new Document(inputText);
+        doc.parseDocument();
+    }
+}
