2ca1219be1c0f675809bc92fc3a89228a202bc80
Anthony <aakin007@odu.edu>
2024-03-20 16:50:22 -0400

    Preped File for future testing for GeoFeatures

---
+++ project/src/test/java/edu/odu/cs/cs350/TestGeographicalFeatures.java
@@ -0,0 +1,19 @@
+package edu.odu.cs.cs350;
+
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class TestGeographicalFeatures {
+    
+    @test
+    public void Test{
+
+    }
+
+}
