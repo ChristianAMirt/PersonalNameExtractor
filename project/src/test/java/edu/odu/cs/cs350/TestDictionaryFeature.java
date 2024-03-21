6dad5652dd4648ed9540c42e50d78f7fc89177ec
Jaylen Wheeler <jwhee010@odu.edu>
2024-03-20 21:03:32 -0400

    Tests for DictionaryFeature should be fine now.

--- project/src/test/java/edu/odu/cs/cs350/TestDictionaryFeature.java
+++ project/src/test/java/edu/odu/cs/cs350/TestDictionaryFeature.java
@@ -1,21 +1,46 @@
 package edu.odu.cs.cs350;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+/**
+ * @author Jaylen Wheeler
+ * 
+ * Tests for the two methods of DictionaryFeature.
+ * (The constructor and "determineDictionaryFeature")
+ */
 public class TestDictionaryFeature {
     
+    /**
+     * Tests the constructor for DictionaryFeature, making sure that
+     * the instantiation of the class of as an object actually happens.
+     */
+    @Test
+    public void testDictionaryFeatureConstructor(){
+        DictionaryFeature firstDictionaryFeature = new DictionaryFeature();
+        assertNotNull(firstDictionaryFeature);
+    }
+
+    /**
+     * Tests the determineDictionaryFeature method for the accuracy
+     * of the returned boolean values.
+     * 
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
     @Test
     public void testDetermineDictionaryFeature() throws FileNotFoundException, IOException
     {
-        DictionaryFeature testForDictionaryFeature = new DictionaryFeature();
+        DictionaryFeature secondDictionaryFeature = new DictionaryFeature();
         Token firstToken = new Token("syntax");
+
+        assertEquals(secondDictionaryFeature.determineDictionaryFeature(firstToken) , true);
         
-        assertEquals(testForDictionaryFeature.determineDictionaryFeature(firstToken) , true);
+        DictionaryFeature thirdDictionaryFeature = new DictionaryFeature();
+        Token secondToken = new Token("bdhiskThisWordShouldNotBeInTheDictionaryJmjhp");
+
+        assertNotEquals(thirdDictionaryFeature.determineDictionaryFeature(secondToken), true);
     }
 }
