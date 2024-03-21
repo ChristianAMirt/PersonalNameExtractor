e64776678026afe0c224db39b2c5f6ff01c74ae0
Anthony <aakin007@odu.edu>
2024-03-20 20:02:04 -0400

    Removed System.exit for no args

--- project/src/main/java/edu/odu/cs/cs350/PNE.java
+++ project/src/main/java/edu/odu/cs/cs350/PNE.java
@@ -1,14 +1,14 @@
 package edu.odu.cs.cs350;
 
 public class PNE {
-    
+
     public static void main(String[] args) {
         if (args.length == 0) {
             System.err.println("PNE.jar [input text]");
-            System.exit(1);
+        } else {
+            String inputText = String.join(" ", args);
+            Document doc = new Document(inputText);
+            doc.parseDocument();
         }
-        String inputText = String.join(" ", args);
-        Document doc = new Document(inputText);
-        doc.parseDocument();
     }
 }
