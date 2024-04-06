fb33478443f587eaa8fa0fef61b706f5c4454371
rsumm003@odu.edu <rsumm003@odu.edu>
2024-04-06 15:10:35 -0400

    changed NameLearningMachine methods to Trainer

--- project/src/main/java/edu/odu/cs/cs350/NameLearningMachine.java
+++ project/src/main/java/edu/odu/cs/cs350/NameLearningMachine.java
@@ -1,65 +1,13 @@
 package edu.odu.cs.cs350;
 
-import weka.core.Instances;
 
 /**
  * The Trainer class is responsible for:
- * - Creating a data set for WEKA
+ * - Use the training model from Trainer
  */
 public class NameLearningMachine {
     
-    //Attributes to look for:
-    //first
-    String[] firstNames;
-    
-    String[] lastNames;
 
-    //honorifics
-
-    //prefixes
-
-    //suffixes
-
-    //killwords
-
-    /* 
-    ****Not sure if we need this...****
-    ****Zeil included it in his code, but WEKA docs say its deprecated****
-
-    FastVector attrInfo = new FastVector();
-    private FastVector fastV(String[] data) {
-        FastVector result = new FastVector(data.length);
-        for (String s: data) {
-            result.addElement(s);
-        }
-        return result;
-    } */
-    
-    /**
-     * Create an ARFF file with attributes and data from raw text file.
-     */
-    public void CreateDataSet() {
-        // Call AccumulateLargeString
-
-        //n-gram process data (aka shingling)
-
-        //apply attributes
-
-        //transform data for Trainer
-    }
-
-    /**
-     * Build a large data stream for processing text file. 
-     */
-    public void AccumulateLargeString(inputStream) {
-        StringBuffer buffer = new StringBuffer;
-        while (!done) {
-            string line = readALineFrom(inputStream);
-            buffer.append(line);
-            done =- moreInputIn(inputStream);
-        }
-        String accumulated = buffer.toString();
-    }
     
 
 }
--- project/src/main/java/edu/odu/cs/cs350/Trainer.java
+++ project/src/main/java/edu/odu/cs/cs350/Trainer.java
@@ -49,7 +49,58 @@ public class Trainer {
 
     }
 
+        //Attributes to look for:
+    //first
+    String[] firstNames;
     
+    String[] lastNames;
+
+    //honorifics
+
+    //prefixes
+
+    //suffixes
+
+    //killwords
+
+    /* 
+    ****Not sure if we need this...****
+    ****Zeil included it in his code, but WEKA docs say its deprecated****
+
+    FastVector attrInfo = new FastVector();
+    private FastVector fastV(String[] data) {
+        FastVector result = new FastVector(data.length);
+        for (String s: data) {
+            result.addElement(s);
+        }
+        return result;
+    } */
+    
+    /**
+     * Create an ARFF file with attributes and data from raw text file.
+     */
+    public void CreateDataSet() {
+        // Call AccumulateLargeString
+
+        //n-gram process data (aka shingling)
+
+        //apply attributes
+
+        //transform data for Trainer
+    }
+
+    /**
+     * Build a large data stream for processing text file. 
+     */
+    public void AccumulateLargeString(inputStream) {
+        StringBuffer buffer = new StringBuffer;
+        while (!done) {
+            string line = readALineFrom(inputStream);
+            buffer.append(line);
+            done =- moreInputIn(inputStream);
+        }
+        String accumulated = buffer.toString();
+    }
 
 
 
