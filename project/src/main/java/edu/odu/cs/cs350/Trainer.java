4cfc6a86ddbe74049db7c20c4cc37566b19e9219
rsumm003@odu.edu <rsumm003@odu.edu>
2024-04-06 14:57:50 -0400

    Training data mush

---
+++ project/src/main/java/edu/odu/cs/cs350/Trainer.java
@@ -0,0 +1,28 @@
+package edu.odu.cs.cs350;
+
+
+/**
+ * The Trainer class is responsible for:
+ * - Running the WEKA training session
+ * - Creating a classifier model for Librarian
+ */
+public class Trainer {
+    
+    double gamma = 0.01;  // initial guess
+    double C = 1.0;       // initial guess
+
+    // Create classifier
+    public void createClassifier(training) {
+        SMO svm = new SMO();        // new classifier instance
+        svm.setOptions(options);    // set the options
+        svm.setKernel(new RBFKernel(training, 25007, gamma));
+        svm.setC(C);
+
+        // Train classifier
+        svm.buildClassifier(training);
+    }
+
+
+
+    
+}
