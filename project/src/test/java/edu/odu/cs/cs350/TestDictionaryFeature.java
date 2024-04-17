cfeb5ab92c310b39da49a536c72adcd1577e513c
Jaylen Wheeler <jwhee010@odu.edu>
2024-04-17 17:41:38 -0400

    Added more DictionaryFeature tests

--- project/src/test/java/edu/odu/cs/cs350/TestDictionaryFeature.java
+++ project/src/test/java/edu/odu/cs/cs350/TestDictionaryFeature.java
@@ -1,22 +1,44 @@
 package edu.odu.cs.cs350;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author Jaylen Wheeler
  * 
- * Tests for the two methods of DictionaryFeature.
- * (The constructor and "determineDictionaryFeature")
+ * Tests for the method of DictionaryFeature
  */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class TestDictionaryFeature{
     
+    /**
+     * DictionaryFeature object that will be used for the testing.
+     */
     private DictionaryFeature myDictionaryFeature;
+
+    /**
+     * Calls the constructor of dictionary feature only
+     * once. This allows for the same DictionaryFeature 
+     * object to be used for every test here.
+     * 
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    @BeforeAll
+    public void callConstructor()throws FileNotFoundException, IOException{
+        myDictionaryFeature = new DictionaryFeature();
+    }
 
     /**
      * Tests the constructor for DictionaryFeature, making sure that
@@ -27,22 +49,38 @@ public class TestDictionaryFeature{
      */
     @Test
     public void testDictionaryFeatureConstructor() throws FileNotFoundException, IOException{
-        myDictionaryFeature = new DictionaryFeature();
         assertNotNull(myDictionaryFeature);
         assertTrue(myDictionaryFeature.getEnglishDictionarySet().contains("tablespoonfuls"));
     }
 
     /**
      * Tests the openFile method for DictionaryFeature to see
-     * if the fileReader is reading a file
+     * if the fileReader is reading a file.
      * @throws FileNotFoundException
      * @throws IOException
      */
     @Test
     public void testOpenFile() throws FileNotFoundException, IOException{
-        myDictionaryFeature = new DictionaryFeature();
         FileReader myReader = myDictionaryFeature.openFile("src/main/data/Dictionary.english.txt");
         assertNotNull(myReader);   
+    }
+
+    /**
+     * Tests the getEnglishDictionarySet method to confirm
+     * that a non-null hashset is returned.
+     */
+    @Test
+    public void testGetEnglishDictionary(){
+        assertNotNull(myDictionaryFeature.getEnglishDictionarySet());
+        assertEquals(HashSet.class, myDictionaryFeature.getEnglishDictionarySet().getClass());
+    }
+
+    @Test
+    public void testLoadFile(){
+        String[] testDictionary = myDictionaryFeature.getEnglishDictionarySet()
+        .toArray(new String[myDictionaryFeature.getEnglishDictionarySet().size()]);
+        //assertTrue()
+        // I am going to finish this
     }
 
     /**
@@ -55,7 +93,6 @@ public class TestDictionaryFeature{
     @Test
     public void testDetermineDictionaryFeature() throws FileNotFoundException, IOException
     {
-        myDictionaryFeature = new DictionaryFeature();
         assertEquals(true , myDictionaryFeature.determineDictionaryFeature("syntax"));
         assertNotEquals(true, myDictionaryFeature.determineDictionaryFeature("bdhiskThisWordShouldNotBeInTheDictionaryJmjhp"));
     }
